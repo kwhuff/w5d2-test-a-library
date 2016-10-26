ddbe484a04a51f75f9975ab2969310da1c4a5344
kwhuff <kwhuff@gmail.com>
2016-10-26 14:17:12 -0400

    second attempt at library tests

--- tests.js
+++ tests.js
@@ -15,13 +15,11 @@ describe('MoDom', function() {
   })
   describe('md.one()', function() {
     it('should select the first element with a given name', function() {
-      md.one('span')
       expect(md.one('span').innerHTML).to.equal('Toggle navigation')
     })
   })
   describe('md.all()', function() {
     it('should select all elements with a given name', function() {
-      md.all('div')
       expect(md.all('div').length).to.be.equal(14)
     })
   })
@@ -40,36 +38,29 @@ describe('MoDom', function() {
   })
   describe('md.hasClass()', function() {
     it('should say whether or not an element has a certain CSS Class', function() {
-      md.hasClass('ul','nav')
-      expect(md.one('ul').classList.contains('nav')).to.be.true
+      expect(md.hasClass('body', 'test')).to.be.true
     })
   })
   describe('md.getAttr()', function() {
-    it('should get an HTML attribute of an element', function() {
-      md.getAttr('input','placeholder')
-      expect(md.one('input').placeholder).to.be.equal('Search...')
-      // md.getAttr('button','class')
-      // expect(md.one('button').classList[1]).to.be.equal('collapsed')
+    it('should return width of image tag as 200', function() {
+      expect(md.getAttr('img', 'width')).to.equal('200')
     })
   })
   describe('md.setAttr()', function() {
-    it('should set an HTML attribute of an element', function() {
-      md.setAttr('input','placeholder','HOLLA')
-      expect(md.one('input').placeholder).to.be.equal('HOLLA')
-      // md.setAttr('button','type','batman')
-      // expect(md.one('button').type).to.be.equal('batman')
+    it('should set img width to 500', function() {
+      md.setAttr('img', 'width', '500')
+      expect(md.getAttr('img', 'width')).to.equal('500')
     })
   })
   describe('md.setHTML()', function() {
-    it('should set the innerHTML of an element', function() {
+    it('should set the innerHTML of div to be BATMAN', function() {
       md.setHTML('div','BATMAN')
       expect(md.one('div').innerHTML).to.be.equal('BATMAN')
     })
   })
   describe('md.getHTML()', function() {
     it('should get the innerHTML of an element', function() {
-      md.getHTML('div')
-      expect(md.one('div').innerHTML).to.be.equal('BATMAN')
+      expect(md.getHTML('div')).to.be.equal('BATMAN')
     })
   })
   describe('md.flipPage()', function() {
